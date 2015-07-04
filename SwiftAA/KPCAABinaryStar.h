dd7d2fd62b6fb16dec8720117ad809d686840863
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-04 09:52:58 +0200

    Adding binary star. One at a time.

---
+++ SwiftAA/KPCAABinaryStar.h
@@ -0,0 +1,43 @@
+//
+//  KPCAABinaryStar.h
+//  SwiftAA
+//
+//  Created by Cédric Foellmi on 04/07/15.
+//  Copyright (c) 2015 onekiloparsec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AABinaryStar.h"
+
+@interface KPCAABinaryStarDetails : NSObject
+
+@property(nonatomic, assign) double r;
+@property(nonatomic, assign) double Theta;
+@property(nonatomic, assign) double Rho;
+
++ (KPCAABinaryStarDetails *)detailsByWrapping:(CAABinaryStarDetails)wrappedDetails;
+
+@end
+
+@interface KPCAABinaryStar : NSObject
+
+/** Units:
+ * t: decimal years
+ * P: mean solar years
+ * T: decimal year
+ * e: n.a.
+ * a: arcseconds
+ * i: degrees
+ * Omega: degrees
+ * w: degrees
+ */
++ (KPCAABinaryStarDetails *)CalculateWithTime:(double)t period:(double)P timeOfPeriastron:(double)T eccentricity:(double)e semimajorAxis:(double)a inclination:(double)i positionAngleOfAscendingNode:(double)Omega longitudeOfPeriastron:(double)w;
+
+/** Units:
+ * e: n.a.
+ * i: degrees
+ * w: degrees
+ */
++ (double)ApparentEccentricityForEccentricity:(double)e inclination:(double)i longitudeOfPeriastron:(double)w;
+
+@end
---
+++ SwiftAA/KPCAABinaryStar.mm
@@ -0,0 +1,55 @@
+//
+//  KPCAABinaryStar.m
+//  SwiftAA
+//
+//  Created by Cédric Foellmi on 04/07/15.
+//  Copyright (c) 2015 onekiloparsec. All rights reserved.
+//
+
+#import "KPCAABinaryStar.h"
+
+@interface KPCAABinaryStarDetails () {
+    CAABinaryStarDetails _wrapped;
+}
+@end
+
+@implementation KPCAABinaryStarDetails
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _wrapped = CAABinaryStarDetails();
+    }
+    return self;
+}
+
+- (instancetype)initWithWrapped:(CAABinaryStarDetails)wrappedDetails
+{
+    self = [super init];
+    if (self) {
+        _wrapped = wrappedDetails;
+    }
+    return self;
+}
+
++ (KPCAABinaryStarDetails *)detailsByWrapping:(CAABinaryStarDetails)wrappedDetails
+{
+    return [[KPCAABinaryStarDetails alloc] initWithWrapped:wrappedDetails];
+}
+
+@end
+
+@implementation KPCAABinaryStar
+
++ (KPCAABinaryStarDetails *)CalculateWithTime:(double)t period:(double)P timeOfPeriastron:(double)T eccentricity:(double)e semimajorAxis:(double)a inclination:(double)i positionAngleOfAscendingNode:(double)Omega longitudeOfPeriastron:(double)w
+{
+    return [KPCAABinaryStarDetails detailsByWrapping:CAABinaryStar::Calculate(t, P, T, e, a, i, Omega, w)];
+}
+
++ (double)ApparentEccentricityForEccentricity:(double)e inclination:(double)i longitudeOfPeriastron:(double)w
+{
+    return CAABinaryStar::ApparentEccentricity(e, i, w);
+}
+
+@end
