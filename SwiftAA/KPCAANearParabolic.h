8da05456f23a964277b4e81320aac67244b81f1f
Cédric Foellmi <cedric@onekilopars.ec>
2015-12-26 09:17:53 +0100

    Near Parabolic : same story (prefix KPC, C function, High precision parameter)

--- SwiftAA/KPCAANearParabolic.h
+++ SwiftAA/KPCAANearParabolic.h
@@ -9,45 +9,35 @@
 #import <Foundation/Foundation.h>
 #import "KPCAA3DCoordinate.h"
 
-@interface KPCAANearParabolicObjectElements : NSObject
-
-@property(nonatomic, assign) double q;
-@property(nonatomic, assign) double i;
-@property(nonatomic, assign) double w;
-@property(nonatomic, assign) double omega;
-@property(nonatomic, assign) double JDEquinox;
-@property(nonatomic, assign) double T;
-@property(nonatomic, assign) double e;
-
-@end
+typedef struct KPCAANearParabolicObjectElements {
+    double q;
+    double i;
+    double w;
+    double omega;
+    double JDEquinox;
+    double T;
+    double e;
+} KPCAANearParabolicObjectElements;
 
 
-@interface KPCAANearParabolicObjectDetails : NSObject
-
-@property(nonatomic, strong) KPCAA3DCoordinate *HeliocentricRectangularEquatorial;
-@property(nonatomic, strong) KPCAA3DCoordinate *HeliocentricRectangularEcliptical;
-@property(nonatomic, assign) double HeliocentricEclipticLongitude;
-@property(nonatomic, assign) double HeliocentricEclipticLatitude;
-@property(nonatomic, assign) double TrueGeocentricRA;
-@property(nonatomic, assign) double TrueGeocentricDeclination;
-@property(nonatomic, assign) double TrueGeocentricDistance;
-@property(nonatomic, assign) double TrueGeocentricLightTime;
-@property(nonatomic, assign) double AstrometricGeocentricRA;
-@property(nonatomic, assign) double AstrometricGeocentricDeclination;
-@property(nonatomic, assign) double AstrometricGeocentricDistance;
-@property(nonatomic, assign) double AstrometricGeocentricLightTime;
-@property(nonatomic, assign) double Elongation;
-@property(nonatomic, assign) double PhaseAngle;
-
-@end
+typedef struct KPCAANearParabolicObjectDetails {
+    KPCAA3DCoordinateComponents HeliocentricRectangularEquatorialCoordinateComponents;
+    KPCAA3DCoordinateComponents HeliocentricRectangularEclipticalCoordinateComponents;
+    double HeliocentricEclipticLongitude;
+    double HeliocentricEclipticLatitude;
+    double TrueGeocentricRA;
+    double TrueGeocentricDeclination;
+    double TrueGeocentricDistance;
+    double TrueGeocentricLightTime;
+    double AstrometricGeocentricRA;
+    double AstrometricGeocentricDeclination;
+    double AstrometricGeocentricDistance;
+    double AstrometricGeocentricLightTime;
+    double Elongation;
+    double PhaseAngle;
+} KPCAANearParabolicObjectDetails;
 
 
-@interface KPCAANearParabolic : NSObject
+KPCAANearParabolicObjectDetails KPCAANearParabolicCalculateObjectDetails(double JD, KPCAANearParabolicObjectElements elements, BOOL highPrecision);
+void KPCAANearParabolicCalulateTrueAnnomalyAndRadius(double JD, struct KPCAANearParabolicObjectElements elements, double *v, double *r);
 
-+ (KPCAANearParabolicObjectDetails *)Calculate:(double)JD elements:(KPCAANearParabolicObjectElements *)elements;
-
-+ (double)cbrt:(double)x;
-
-+ (void)CalulateTrueAnnomalyAndRadius:(double)JD elements:(KPCAANearParabolicObjectElements *)elements v:(double *)v r:(double *)r;
-
-@end
--- SwiftAA/KPCAANearParabolic.mm
+++ SwiftAA/KPCAANearParabolic.mm
@@ -9,309 +9,56 @@
 #import "KPCAANearParabolic.h"
 #import "AANearParabolic.h"
 
-@interface KPCAANearParabolicObjectElements () {
-    CAANearParabolicObjectElements _wrapped;
-}
-- (CAANearParabolicObjectElements)wrappedElements;
-@end
-
-@implementation KPCAANearParabolicObjectElements
-
-- (instancetype)init
+KPCAANearParabolicObjectDetails KPCAANearParabolicCalculateObjectDetails(double JD, KPCAANearParabolicObjectElements elements, BOOL highPrecision)
 {
-    self = [super init];
-    if (self) {
-        _wrapped = CAANearParabolicObjectElements();
-    }
-    return self;
+    CAANearParabolicObjectElements elementsPlus = CAANearParabolicObjectElements();
+
+    elementsPlus.q = elements.q;
+    elementsPlus.i = elements.i;
+    elementsPlus.w = elements.w;
+    elementsPlus.omega = elements.omega;
+    elementsPlus.JDEquinox = elements.JDEquinox;
+    elementsPlus.T = elements.T;
+    elementsPlus.e = elements.e;
+    
+    CAANearParabolicObjectDetails detailsPlus = CAANearParabolic::Calculate(JD, elementsPlus, highPrecision);
+    
+    struct KPCAANearParabolicObjectDetails details;
+    
+    details.HeliocentricRectangularEclipticalCoordinateComponents = KPCAA3DCoordinateComponentsMake(detailsPlus.HeliocentricRectangularEcliptical.X,
+                                                                                                    detailsPlus.HeliocentricRectangularEcliptical.Y,
+                                                                                                    detailsPlus.HeliocentricRectangularEcliptical.Z);
+    
+    details.HeliocentricRectangularEquatorialCoordinateComponents = KPCAA3DCoordinateComponentsMake(detailsPlus.HeliocentricRectangularEquatorial.X,
+                                                                                                    detailsPlus.HeliocentricRectangularEquatorial.Y,
+                                                                                                    detailsPlus.HeliocentricRectangularEquatorial.Z);
+    
+    details.HeliocentricEclipticLongitude = detailsPlus.HeliocentricEclipticLongitude;
+    details.HeliocentricEclipticLatitude = detailsPlus.HeliocentricEclipticLatitude;
+    details.TrueGeocentricRA = detailsPlus.TrueGeocentricRA;
+    details.TrueGeocentricDeclination = detailsPlus.TrueGeocentricDeclination;
+    details.TrueGeocentricDistance = detailsPlus.TrueGeocentricDistance;
+    details.TrueGeocentricLightTime = detailsPlus.TrueGeocentricLightTime;
+    details.AstrometricGeocentricRA = detailsPlus.AstrometricGeocentricRA;
+    details.AstrometricGeocentricDeclination = detailsPlus.AstrometricGeocentricDeclination;
+    details.AstrometricGeocentricDistance = detailsPlus.AstrometricGeocentricDistance;
+    details.AstrometricGeocentricLightTime = detailsPlus.AstrometricGeocentricLightTime;
+    details.Elongation = detailsPlus.Elongation;
+    details.PhaseAngle = detailsPlus.PhaseAngle;
+    
+    return details;
 }
 
-- (instancetype)initWithWrappedElements:(CAANearParabolicObjectElements)wrappedElements
+void KPCAANearParabolicCalulateTrueAnnomalyAndRadius(double JD, struct KPCAANearParabolicObjectElements elements, double *v, double *r)
 {
-    self = [super init];
-    if (self) {
-        _wrapped = wrappedElements;
-    }
-    return self;
+    CAANearParabolicObjectElements elementsPlus;
+    CAANearParabolic::CalulateTrueAnnomalyAndRadius(JD, elementsPlus, *v, *r);
+    
+    elements.q = elementsPlus.q;
+    elements.i = elementsPlus.i;
+    elements.w = elementsPlus.w;
+    elements.omega = elementsPlus.omega;
+    elements.JDEquinox = elementsPlus.JDEquinox;
+    elements.T = elementsPlus.T;
+    elements.e = elementsPlus.e;
 }
-
-+ (KPCAANearParabolicObjectElements *)elementsByWrapping:(CAANearParabolicObjectElements)wrappedElements
-{
-    return [[KPCAANearParabolicObjectElements alloc] initWithWrappedElements:wrappedElements];
-}
-
-- (CAANearParabolicObjectElements)wrappedElements
-{
-    return _wrapped;
-}
-
-- (double)q
-{
-    return _wrapped.q;
-}
-
-- (void)setQ:(double)q
-{
-    _wrapped.q = q;
-}
-
-- (double)i
-{
-    return _wrapped.i;
-}
-
-- (void)setI:(double)i
-{
-    _wrapped.i = i;
-}
-
-- (double)w
-{
-    return _wrapped.w;
-}
-
-- (void)setW:(double)w
-{
-    _wrapped.w = w;
-}
-
-- (double)omega
-{
-    return _wrapped.omega;
-}
-
-- (void)setOmega:(double)omega
-{
-    _wrapped.omega = omega;
-}
-
-- (double)JDEquinox
-{
-    return _wrapped.JDEquinox;
-}
-
-- (void)setJDEquinox:(double)JDEquinox
-{
-    _wrapped.JDEquinox = JDEquinox;
-}
-
-- (double)T
-{
-    return _wrapped.T;
-}
-
-- (void)setT:(double)T
-{
-    _wrapped.T = T;
-}
-
-- (double)e
-{
-    return _wrapped.e;
-}
-
-- (void)setE:(double)e
-{
-    _wrapped.e = e;
-}
-
-@end
-
-
-@interface KPCAA3DCoordinate ()
-+ (KPCAA3DCoordinate *)coordinateByWrapping:(CAA3DCoordinate)wrappedCoord;
-- (CAA3DCoordinate)wrappedCoord;
-@end
-
-@interface KPCAANearParabolicObjectDetails () {
-    CAANearParabolicObjectDetails _wrapped;
-}
-@end
-
-@implementation KPCAANearParabolicObjectDetails
-
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        _wrapped = CAANearParabolicObjectDetails();
-    }
-    return self;
-}
-
-- (instancetype)initWithWrappedDetails:(CAANearParabolicObjectDetails)wrappedDetails
-{
-    self = [super init];
-    if (self) {
-        _wrapped = wrappedDetails;
-    }
-    return self;
-}
-
-+ (KPCAANearParabolicObjectDetails *)detailsByWrapping:(CAANearParabolicObjectDetails)wrappedDetails
-{
-    return [[KPCAANearParabolicObjectDetails alloc] initWithWrappedDetails:wrappedDetails];
-}
-
-- (KPCAA3DCoordinate *)HeliocentricRectangularEquatorial
-{
-    return [KPCAA3DCoordinate coordinateByWrapping:_wrapped.HeliocentricRectangularEquatorial];
-}
-
-- (void)setHeliocentricRectangularEquatorial:(KPCAA3DCoordinate *)HeliocentricRectangularEquatorial
-{
-    _wrapped.HeliocentricRectangularEquatorial = HeliocentricRectangularEquatorial.wrappedCoord;
-}
-
-- (KPCAA3DCoordinate *)HeliocentricRectangularEcliptical
-{
-    return [KPCAA3DCoordinate coordinateByWrapping:_wrapped.HeliocentricRectangularEcliptical];
-}
-
-- (void)setHeliocentricRectangularEcliptical:(KPCAA3DCoordinate *)HeliocentricRectangularEcliptical
-{
-    _wrapped.HeliocentricRectangularEcliptical = HeliocentricRectangularEcliptical.wrappedCoord;
-}
-
-- (double)HeliocentricEclipticLongitude
-{
-    return _wrapped.HeliocentricEclipticLongitude;
-}
-
-- (void)setHeliocentricEclipticLongitude:(double)HeliocentricEclipticLongitude
-{
-    _wrapped.HeliocentricEclipticLongitude = HeliocentricEclipticLongitude;
-}
-
-- (double)HeliocentricEclipticLatitude
-{
-    return _wrapped.HeliocentricEclipticLatitude;
-}
-
-- (void)setHeliocentricEclipticLatitude:(double)HeliocentricEclipticLatitude
-{
-    _wrapped.HeliocentricEclipticLatitude = HeliocentricEclipticLatitude;
-}
-
-- (double)TrueGeocentricRA
-{
-    return _wrapped.TrueGeocentricRA;
-}
-
-- (void)setTrueGeocentricRA:(double)TrueGeocentricRA
-{
-    _wrapped.TrueGeocentricRA = TrueGeocentricRA;
-}
-
-- (double)TrueGeocentricDeclination
-{
-    return _wrapped.TrueGeocentricDeclination;
-}
-
-- (void)setTrueGeocentricDeclination:(double)TrueGeocentricDeclination
-{
-    _wrapped.TrueGeocentricDeclination = TrueGeocentricDeclination;
-}
-
-- (double)TrueGeocentricDistance
-{
-    return _wrapped.TrueGeocentricDistance;
-}
-
-- (void)setTrueGeocentricDistance:(double)TrueGeocentricDistance
-{
-    _wrapped.TrueGeocentricDistance = TrueGeocentricDistance;
-}
-
-- (double)TrueGeocentricLightTime
-{
-    return _wrapped.TrueGeocentricLightTime;
-}
-
-- (void)setTrueGeocentricLightTime:(double)TrueGeocentricLightTime
-{
-    _wrapped.TrueGeocentricLightTime = TrueGeocentricLightTime;
-}
-
-- (double)AstrometricGeocentricRA
-{
-    return _wrapped.AstrometricGeocentricRA;
-}
-
-- (void)setAstrometricGeocentricRA:(double)AstrometricGeocentricRA
-{
-    _wrapped.AstrometricGeocentricRA = AstrometricGeocentricRA;
-}
-
-- (double)AstrometricGeocentricDeclination
-{
-    return _wrapped.AstrometricGeocentricDeclination;
-}
-
-- (void)setAstrometricGeocentricDeclination:(double)AstrometricGeocentricDeclination
-{
-    _wrapped.AstrometricGeocentricDeclination = AstrometricGeocentricDeclination;
-}
-
-- (double)AstrometricGeocentricDistance
-{
-    return _wrapped.AstrometricGeocentricDistance;
-}
-
-- (void)setAstrometricGeocentricDistance:(double)AstrometricGeocentricDistance
-{
-    _wrapped.AstrometricGeocentricDistance = AstrometricGeocentricDistance;
-}
-
-- (double)AstrometricGeocentricLightTime
-{
-    return _wrapped.AstrometricGeocentricLightTime;
-}
-
-- (void)setAstrometricGeocentricLightTime:(double)AstrometricGeocentricLightTime
-{
-    _wrapped.AstrometricGeocentricLightTime = AstrometricGeocentricLightTime;
-}
-
-- (double)Elongation
-{
-    return _wrapped.Elongation;
-}
-
-- (void)setElongation:(double)Elongation
-{
-    _wrapped.Elongation = Elongation;
-}
-
-- (double)PhaseAngle
-{
-    return _wrapped.PhaseAngle;
-}
-
-- (void)setPhaseAngle:(double)PhaseAngle
-{
-    _wrapped.PhaseAngle = PhaseAngle;
-}
-
-@end
-
-
-
-@implementation KPCAANearParabolic
-
-+ (KPCAANearParabolicObjectDetails *)Calculate:(double)JD elements:(KPCAANearParabolicObjectElements *)elements
-{
-    return [KPCAANearParabolicObjectDetails detailsByWrapping:CAANearParabolic::Calculate(JD, elements.wrappedElements)];
-}
-
-+ (double)cbrt:(double)x
-{
-    return CAANearParabolic::cbrt(x);
-}
-
-+ (void)CalulateTrueAnnomalyAndRadius:(double)JD elements:(KPCAANearParabolicObjectElements *)elements v:(double *)v r:(double *)r
-{
-    return CAANearParabolic::CalulateTrueAnnomalyAndRadius(JD, elements.wrappedElements, *v, *r);
-}
-
-@end
