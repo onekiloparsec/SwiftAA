b2e0b60a5d1b74c6547f06408597caf356454fbc
Cédric <cedric@onekilopars.ec>
2016-07-17 22:35:21 +0200

    Much improved Playground to start like a book or a course...

---
+++ SwiftAA/SwiftAA.playground/Pages/Intro.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+import SwiftAA
+import XCPlayground
+
+//: # Swift AA Playground
+//: ### A world to play with at your fingertips!
+//: ### Developed by Cédric Foellmi, Ph.D. ([@onekiloparsec](https://twitter.com/onekiloparsec))
+//:
+//: ----
+//:
+//: ## Foreword
+//: SwiftAA is the most complete and accurate set of astronomical algorithms available for Swift, period.
+//: It originates from the development of a professionnal app called [iObserve](http://onekilopars.ec/iobserve)
+//: and available since 2010 on the Mac.
+//: This app has also been ported to iPad, has been downloaded thousands of times around the world, and is used in all
+//: major observatories in the world, from world-class VLT, Keck, Gemini to smaller ones in people's backyard.
+//: At some point the natural wish of making a much better version 2 of this app appeared. To do so, some
+//: new UI components would need to be developed, as well as a much improved set of algorithms. Interestingly, 
+//: the Swift language just appaeared at similar epoch.
+//:
+//: I had already quite some experience with implementing astronomical algorithms from the reference textbook
+//: *Astronomical Algorithms* (AA) written by Jean Meeus. But with ambitious goals for iObserve2, I would need to
+//: implement lots of them. And a good programmer knows when to reuse code... So I contacted Paul J. Naughter, whose
+//: [website](http://www.naughter.com/aa.html) 
+//: offer precisely this: a fairly complete implementation of AA algorithms, called **AA+**. 
+//: He agreed to let me put his code
+//: on a GitHub repository and develop my own solution, as long as its code retained the licence, and he remains the
+//: only master source of AA+ code. **So here comes SwiftAA!**
+//: 
+//: SwiftAA was first developed for an easy use in the development of apps, among which the coming iObserve2. 
+//: But with the advent of Playgrounds, SwiftAA could become a lot more interesting. It could become a truly
+//: **educational tool**. Hence this Playground. Ultimately, my dream would be to see the SwiftAA playground
+//: become a kind of some highschool course. With examples, graphs, animations...
+//:
+//: To achieve this goal, SwiftAA has been exented to not only contains AA+ algorithms, but also much more expressive
+//: APIs and functions and conversions and all that makes the use of Swift a lot simpler and fun.
+//:
+//: ## A note on VSOP87
+//:
+//: ## A note on Code Style and Conventions
+//:
+//:
+//: [Next](@next)
--- SwiftAA/SwiftAA.playground/Sources/SolarSystemView.swift
+++ SwiftAA/SwiftAA.playground/Sources/SolarSystemView.swift
@@ -96,3 +96,32 @@ public class SolarSystemView : UIView {
         }        
     }
 }
+
+// Usage: 
+//let scale = 100.0 // pixels/AU
+//
+//var solarSystemView = SolarSystemView()
+//XCPlaygroundPage.currentPage.liveView = solarSystemView
+//
+//for i in 0..<100 {
+//    let jd = KPCAADate(year: 2016, month: 6, day: 21.0, usingGregorianCalendar: true).Julian() + Double(i)
+//    
+//    let planets: Array<Planet> = [Mars(julianDay: jd)]
+//    let name = String(planets[0].dynamicType)
+//    print("\(name)")
+//    
+//    for (index, planet) in planets.enumerate() {
+//        var r = planet.radiusVector
+//        var phi = planet.eclipticLongitude
+//        var x = r * cos(phi.Radians)
+//        var y = r * sin(phi.Radians)
+//        
+//        solarSystemView.drawPlanet(planet.name, withColor:planet.dynamicType.color, atPosition: CGPoint(x: x*scale, y: y*scale));
+//        
+//        //        if (index == 0) {
+//        //            Swift.print("\(KPCAADate()) \(jd) \(x) \(y) \(r)")
+//        //        }
+//        
+//    }
+//}
+//
