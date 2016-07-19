c70700f1d5d2de23c4782efbc35e165dfa5ec4b6
Cédric <cedric@onekilopars.ec>
2016-07-19 21:42:23 +0200

    Documentation

--- SwiftAA/JulianDay.swift
+++ SwiftAA/JulianDay.swift
@@ -16,10 +16,24 @@ public extension JulianDay {
         return KPCAADate(julianDay: self, usingGregorianCalendar: true)
     }
     
+    /**
+     Computes the mean sidereal time for the Greenwich meridian.
+     That is, the Greenwich hour angle of the mean vernal point (the intersection of the ecliptic
+     of the date with the mean equator of the date).
+     
+     - returns: The sidereal time in hours.
+     */
     func MeanGreenwichSiderealTime() -> Hour {
         return KPCAASidereal_MeanGreenwichSiderealTime(self)
     }
-    
+
+    /**
+     Computes the apparent sidereal time.
+     That is, the Greenwich hour angle of the true vernal equinox, obtained by adding a correction
+     that depends on the nutation in longitude, and the true obliquity of the ecliptic.
+     
+     - returns: The sidereal time in hours.
+     */
     func ApparentGreenwichSiderealTime() -> Hour {
         return KPCAASidereal_ApparentGreenwichSiderealTime(self)
     }
