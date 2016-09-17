9f5b104ab1682459ac7a0693ba097bebb1cc100e
Cédric <cedric@onekilopars.ec>
2016-09-17 16:24:45 +0200

    Julian Day tests

--- SwiftAATests/JulianDayTest.swift
+++ SwiftAATests/JulianDayTest.swift
@@ -20,7 +20,26 @@ class JulianDayTest: XCTestCase {
         super.tearDown()
     }
 
-    func testDateToJulianDay() {
+    func testDate1ToJulianDay() {
+        var components = DateComponents()
+        components.year = 2016
+        components.month = 9
+        components.day = 17
+        let date = Calendar(identifier: .gregorian).date(from: components)
+        XCTAssertEqual(date?.julianDay(), 2457648.500000)
+    }
+
+    func testDate2ToJulianDay() {
+        var components = DateComponents()
+        components.year = 1916
+        components.month = 9
+        components.day = 17
+        components.hour = 2
+        components.minute = 3
+        components.second = 4
+        components.nanosecond = 500000
+        let date = Calendar(identifier: .gregorian).date(from: components)!
+        XCTAssertEqualWithAccuracy(date.julianDay(), 2421123.585469, accuracy: 0.000001)
     }
 
     func testJulianDayToDateComponents() {
