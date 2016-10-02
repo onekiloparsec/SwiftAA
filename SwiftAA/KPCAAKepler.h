101f2654488a97a87ccd0602e7635b8f7b3646d1
Cédric <cedric@onekilopars.ec>
2016-10-02 23:12:03 +0200

    doc

--- SwiftAA/KPCAAKepler.h
+++ SwiftAA/KPCAAKepler.h
@@ -13,6 +13,8 @@ extern "C" {
 #endif
 
 // Pass nIterations=0 to use default (53).
+// M = mean anomaly
+// e = eccentricity of planet orbit.
 double KPCAAKepler_Calculate(double M, double e, int nIterations);
 
 #if __cplusplus
