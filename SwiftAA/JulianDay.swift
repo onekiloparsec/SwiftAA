95e702cb3a6722e28e4cd839ff821f7bcb766575
Alexander Vasenin <alex.vasenin@me.com>
2016-12-21 00:29:30 +0300

    added special cases for J2000 and B1950

--- SwiftAA/JulianDay.swift
+++ SwiftAA/JulianDay.swift
@@ -226,7 +226,13 @@ public extension Date {
 }
 
 extension JulianDay: CustomStringConvertible {
-    public var description: String { return String(format: "JD %.2f", value) }
+    public var description: String {
+        switch self {
+        case StandardEpoch_J2000_0: return "J2000.0"
+        case StandardEpoch_B1950_0: return "B1950.0"
+        default: return String(format: "JD %.2f", value)
+        }
+    }
 }
 
 
