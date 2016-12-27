98b1db365a0cbdab4c40d5f37973158804e7c538
Alexander Vasenin <alex.vasenin@me.com>
2016-12-27 07:50:36 +0300

    improved AstronomicalCoordinatesTests

--- SwiftAATests/AstronomicalCoordinatesTests.swift
+++ SwiftAATests/AstronomicalCoordinatesTests.swift
@@ -12,35 +12,35 @@ import XCTest
 class AstronomicalCoordinatesTests: XCTestCase {
     
     func testEquatorial2Ecliptic() { // p.95
-        let equatorial = EquatorialCoordinates(alpha: Hour(7.0 + 45.0/60.0 + 18.946/3600.0), delta: Degree(28.0 + 1.0/60.0 + 34.26/3600.0))
+        let equatorial = EquatorialCoordinates(alpha: Hour(7, 45, 18.946), delta: Degree(28, 1, 34.26))
         let ecliptic = equatorial.toEclipticCoordinates()
-        XCTAssertEqualWithAccuracy(ecliptic.lambda.value, 113.215630, accuracy: 0.01/3600.0)
-        XCTAssertEqualWithAccuracy(ecliptic.beta.value, 6.684170, accuracy: 0.01/3600.0)
+        AssertEqual(ecliptic.lambda, Degree(113.215630), accuracy: ArcSecond(0.01).inDegrees)
+        AssertEqual(ecliptic.beta, Degree(6.684170), accuracy: ArcSecond(0.01).inDegrees)
         let eqBack = ecliptic.toEquatorialCoordinates()
-        XCTAssertEqualWithAccuracy(eqBack.rightAscension.value, equatorial.rightAscension.value, accuracy: 0.1/3600.0)
-        XCTAssertEqualWithAccuracy(eqBack.declination.value, equatorial.declination.value, accuracy: 0.1/3600.0)
+        AssertEqual(eqBack.rightAscension, equatorial.rightAscension, accuracy: ArcSecond(0.01).inHours)
+        AssertEqual(eqBack.declination, equatorial.declination, accuracy: ArcSecond(0.01).inDegrees)
     }
     
     func testEquatorial2Horizontal() { // p.95
-        let date = Calendar.gregorianGMT.date(from: DateComponents(year: 1987, month: 04, day: 10, hour: 19, minute: 21, second: 00))!
-        let equatorial = EquatorialCoordinates(alpha: Hour(23.0 + 9.0/60.0 + 16.641/3600.0), delta: Degree( -6.0 - 43.0/60.0 - 11.61/3600.0))
-        let geographic = GeographicCoordinates(positivelyWestwardLongitude: Degree(77.0 + 3.0/60.0 + 56.0/3600.0), latitude: Degree(38.0 + 55.0/60.0 + 17.0/3600.0))
-        let horizontal = equatorial.toHorizontalCoordinates(forGeographicalCoordinates: geographic, julianDay: date.julianDay)
-        XCTAssertEqualWithAccuracy(horizontal.altitude.value, 15.1249, accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(horizontal.azimuth.value, 68.0337, accuracy: 0.001)
+        let jd = JulianDay(year: 1987, month: 4, day: 10, hour: 19, minute: 21, second: 0)
+        let equatorial = EquatorialCoordinates(alpha: Hour(23, 9, 16.641), delta: Degree(-6, -43, -11.61))
+        let geographic = GeographicCoordinates(positivelyWestwardLongitude: Degree(77, 3, 56.0), latitude: Degree(38, 55, 17.0))
+        let horizontal = equatorial.toHorizontalCoordinates(forGeographicalCoordinates: geographic, julianDay: jd)
+        AssertEqual(horizontal.altitude, Degree(15.1249), accuracy: ArcSecond(5.0).inDegrees)
+        AssertEqual(horizontal.azimuth, Degree(68.0337), accuracy: ArcSecond(5.0).inDegrees)
         let eqBack = horizontal.toEquatorialCoordinates()
-        XCTAssertEqualWithAccuracy(eqBack.rightAscension.value, equatorial.rightAscension.value, accuracy: 0.1/3600.0)
-        XCTAssertEqualWithAccuracy(eqBack.declination.value, equatorial.declination.value, accuracy: 0.1/3600.0)
+        AssertEqual(eqBack.rightAscension, equatorial.rightAscension, accuracy: ArcSecond(0.01).inHours)
+        AssertEqual(eqBack.declination, equatorial.declination, accuracy: ArcSecond(0.01).inDegrees)
     }
     
     func testEquatorial2Galactic() { // p.95
-        let equatorial = EquatorialCoordinates(alpha: Hour(17.0 + 48.0/60.0 + 59.74/3600.0), delta: Degree( -14.0 - 43.0/60.0 - 8.2/3600.0), epsilon: StandardEpoch_B1950_0)
+        let equatorial = EquatorialCoordinates(alpha: Hour(17, 48, 59.74), delta: Degree(-14, -43, -8.2), epsilon: StandardEpoch_B1950_0)
         let galactic = equatorial.toGalacticCoordinates()
-        XCTAssertEqualWithAccuracy(galactic.l.value, 12.9593, accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(galactic.b.value, 6.0463, accuracy: 0.001)
+        AssertEqual(galactic.l, Degree(12.9593), accuracy: ArcSecond(1.0).inDegrees)
+        AssertEqual(galactic.b, Degree(6.0463), accuracy: ArcSecond(1.0).inDegrees)
         let eqBack = galactic.toEquatorialCoordinates()
-        XCTAssertEqualWithAccuracy(eqBack.rightAscension.value, equatorial.rightAscension.value, accuracy: 0.1/3600.0)
-        XCTAssertEqualWithAccuracy(eqBack.declination.value, equatorial.declination.value, accuracy: 0.1/3600.0)
+        AssertEqual(eqBack.rightAscension, equatorial.rightAscension, accuracy: ArcSecond(0.01).inHours)
+        AssertEqual(eqBack.declination, equatorial.declination, accuracy: ArcSecond(0.01).inDegrees)
     }
     
 }
