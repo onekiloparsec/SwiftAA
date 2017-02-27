1ba5bb75127a71fba83935f1755188c3a15e5fb6
Cédric <cedric@onekilopars.ec>
2017-02-27 14:37:26 +0100

    Unit tests for Magnitudes

--- SwiftAA/Magnitudes.swift
+++ SwiftAA/Magnitudes.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 public extension Magnitude {
-    public func combine(withMagnitude m2: Magnitude) -> Magnitude {
+    public func combine(with m2: Magnitude) -> Magnitude {
         return Magnitude(KPCAAStellarMagnitudes_CombinedMagnitude(self.value, m2.value))
     }
     
-    public func brightnessRatio(withMagnitude m2: Magnitude) -> Double {
+    public func brightnessRatio(with m2: Magnitude) -> Double {
         return KPCAAStellarMagnitudes_BrightnessRatio(self.value, m2.value)
     }
 
-    public func distance(forAbsoluteMagnitude M: Magnitude, visualAbsorption Av: Double = 0.0) -> Parsec {
-        return Parsec(pow(10.0, (self.value + 5.0 - M.value - Av)/5.0))
+    public func distance(forAbsoluteMagnitude M: Magnitude, visualAbsorption Av: Magnitude = 0.0) -> Parsec {
+        return Parsec(pow(10.0, (self.value + 5.0 - M.value - Av.value)/5.0))
     }
     
-    static public func magnitudeDifference(forBrightnessRatio r: Double) -> Double {
-        return KPCAAStellarMagnitudes_MagnitudeDifference(r)
+    static public func magnitudeDifference(forBrightnessRatio r: Double) -> Magnitude {
+        return Magnitude(KPCAAStellarMagnitudes_MagnitudeDifference(r))
     }
 }
--- SwiftAATests/MagnitudeTests.swift
+++ SwiftAATests/MagnitudeTests.swift
@@ -7,29 +7,38 @@
 //
 
 import XCTest
+@testable import SwiftAA
 
 class MagnitudeTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMagnitudeCombination() {
+        let m = Magnitude(1.0)
+        let m1 = Magnitude(2.0)
+        let m2 = Magnitude(0.0)
+        let m3 = Magnitude(-2.0)
+        AssertEqual(m.combine(with: m1),  0.636148, accuracy: 0.000001)
+        AssertEqual(m.combine(with: m2), -0.363851, accuracy: 0.000001)
+        AssertEqual(m.combine(with: m3), -2.066430, accuracy: 0.000001)
+    }
+
+    func testMagnitudeBrightnessRatio() {
+        let m = Magnitude(1.0)
+        let m1 = Magnitude(2.0)
+        let m2 = Magnitude(0.0)
+        let m3 = Magnitude(-2.0)
+        XCTAssertEqualWithAccuracy(m.brightnessRatio(with: m1), 2.511886, accuracy: 0.000001)
+        XCTAssertEqualWithAccuracy(m.brightnessRatio(with: m2), 0.398107, accuracy: 0.000001)
+        XCTAssertEqualWithAccuracy(m.brightnessRatio(with: m3), 0.063095, accuracy: 0.000001)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testMagnitudeDistance() {
+        let m = Magnitude(14.2)
+        let M = Magnitude(-5.0)
+        let Av = Magnitude(9.2)
+        AssertEqual(m.distance(forAbsoluteMagnitude: M, visualAbsorption: Av), 1000.0, accuracy: 1.0) // hé hé hé ... https://arxiv.org/abs/0812.4232
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testMagnitudeDifference() {
+        XCTAssertEqual(Magnitude.magnitudeDifference(forBrightnessRatio: 2.51188643150958),  1.0)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
