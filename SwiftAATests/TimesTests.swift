e3dde938fbf847f5cd0307864d12521ba8579e31
Cédric <cedric@onekilopars.ec>
2017-03-17 21:59:04 +0100

    More tests
    Signed-off-by: Cédric <cedric@onekilopars.ec>

--- SwiftAATests/TimesTests.swift
+++ SwiftAATests/TimesTests.swift
@@ -59,4 +59,46 @@ class TimesTests: XCTestCase {
         let hminussexagesimal: SexagesimalNotation = (.minus, 1, 7, 30.0)
         XCTAssertTrue(hminus.sexagesimalNotation == hminussexagesimal)
     }
+    
+    // See AATests.cpp
+    func testTTtoUTRoundTripping() {
+        let earth = Earth(julianDay: JulianDay(year: 1962, month: 1, day: 1), highPrecision: false)
+        let northwardEquinox = earth.equinox(true)
+        XCTAssertEqual(northwardEquinox.value, JulianDay(2437744.6042503607).value)
+        
+        // TT
+        XCTAssertEqual(northwardEquinox, JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 30, second: 7.231168))
+
+        // TT -> UT
+        XCTAssertEqual(northwardEquinox.TTtoUTC(), JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 29, second: 33.112424))
+
+        // TT -> UT -> TT
+        XCTAssertEqual(northwardEquinox.TTtoUTC().UTCtoTT(), JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 30, second: 7.231168))
+    }
+    
+    // See AATests.cpp
+    func testTTtoTAIRoundTripping() {
+        let earth = Earth(julianDay: JulianDay(year: 1962, month: 1, day: 1), highPrecision: false)
+        let northwardEquinox = earth.equinox(true)
+        XCTAssertEqual(northwardEquinox.value, JulianDay(2437744.6042503607).value)
+        
+        // TT -> TAI
+        XCTAssertEqual(northwardEquinox.TTtoTAI(), JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 29, second: 35.047155))
+        
+        // TT -> TAI -> TT
+        XCTAssertEqual(northwardEquinox.TTtoTAI().TAItoTT(), JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 30, second: 7.231168))
+    }
+    
+    // See AATests.cpp
+    func testTTtoUT1RoundTripping() {
+        let earth = Earth(julianDay: JulianDay(year: 1962, month: 1, day: 1), highPrecision: false)
+        let northwardEquinox = earth.equinox(true)
+        XCTAssertEqual(northwardEquinox.value, JulianDay(2437744.6042503607).value)
+        
+        // TT -> UT1
+        XCTAssertEqual(northwardEquinox.TTtoUT1(), JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 29, second: 33.140024))
+        
+        // TT -> UT1 -> TT
+        XCTAssertEqual(northwardEquinox.TTtoUT1().UT1toTT(), JulianDay(year: 1962, month: 3, day: 21, hour: 2, minute: 30, second: 7.231168))
+    }
 }
