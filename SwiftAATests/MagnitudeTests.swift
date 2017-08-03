b0a243adf424c7aa55755b9ccaa3e37754ab8ef4
Roderic Campbell <roderic@gmail.com>
2017-08-02 17:42:44 -0700

    Update tests with new SpaceMagnitude
    re #58

--- SwiftAATests/MagnitudeTests.swift
+++ SwiftAATests/MagnitudeTests.swift
@@ -12,33 +12,33 @@ import XCTest
 class MagnitudeTests: XCTestCase {
 
     func testMagnitudeCombination() {
-        let m = Magnitude(1.0)
-        let m1 = Magnitude(2.0)
-        let m2 = Magnitude(0.0)
-        let m3 = Magnitude(-2.0)
+        let m = SpaceMagnitude(1.0)
+        let m1 = SpaceMagnitude(2.0)
+        let m2 = SpaceMagnitude(0.0)
+        let m3 = SpaceMagnitude(-2.0)
         AssertEqual(m.combine(with: m1),  0.636148, accuracy: 0.000001)
         AssertEqual(m.combine(with: m2), -0.363851, accuracy: 0.000001)
         AssertEqual(m.combine(with: m3), -2.066430, accuracy: 0.000001)
     }
 
     func testMagnitudeBrightnessRatio() {
-        let m = Magnitude(1.0)
-        let m1 = Magnitude(2.0)
-        let m2 = Magnitude(0.0)
-        let m3 = Magnitude(-2.0)
+        let m = SpaceMagnitude(1.0)
+        let m1 = SpaceMagnitude(2.0)
+        let m2 = SpaceMagnitude(0.0)
+        let m3 = SpaceMagnitude(-2.0)
         XCTAssertEqualWithAccuracy(m.brightnessRatio(with: m1), 2.511886, accuracy: 0.000001)
         XCTAssertEqualWithAccuracy(m.brightnessRatio(with: m2), 0.398107, accuracy: 0.000001)
         XCTAssertEqualWithAccuracy(m.brightnessRatio(with: m3), 0.063095, accuracy: 0.000001)
     }
     
     func testMagnitudeDistance() {
-        let m = Magnitude(14.2)
-        let M = Magnitude(-5.0)
-        let Av = Magnitude(9.2)
+        let m = SpaceMagnitude(14.2)
+        let M = SpaceMagnitude(-5.0)
+        let Av = SpaceMagnitude(9.2)
         AssertEqual(m.distance(forAbsoluteMagnitude: M, visualAbsorption: Av), 1000.0, accuracy: 1.0) // hé hé hé ... https://arxiv.org/abs/0812.4232
     }
     
     func testMagnitudeDifference() {
-        XCTAssertEqual(Magnitude.magnitudeDifference(forBrightnessRatio: 2.51188643150958),  1.0)
+        XCTAssertEqual(SpaceMagnitude.magnitudeDifference(forBrightnessRatio: 2.51188643150958),  1.0)
     }
 }
