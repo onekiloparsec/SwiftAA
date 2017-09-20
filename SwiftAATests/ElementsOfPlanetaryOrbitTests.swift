f8d68d2a1bc868fd4977b33cf9356f0a6a7b08ff
onekiloparsec <cedric@onekilopars.ec>
2017-09-20 18:50:13 +0200

    Tests of planetary orbits, finally.
    Signed-off-by: onekiloparsec <cedric@onekilopars.ec>

---
+++ SwiftAATests/ElementsOfPlanetaryOrbitTests.swift
@@ -0,0 +1,29 @@
+//
+//  ElementsOfPlanetaryOrbitTests.swift
+//  SwiftAA
+//
+//  Created by Cédric Foellmi on 2017-09-20.
+//  Copyright © 2017 onekiloparsec. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftAA
+
+class ElementsOfPlanetaryOrbitTests: XCTestCase {
+
+    // See AA p.211, Example 31.a
+    func testMeanOrbitalElementsOfMercury() {
+        let jd = JulianDay(2475460.5)
+        let mercury = Mercury(julianDay: jd, highPrecision: false)
+
+        // numeric types
+        AssertEqual(mercury.semimajorAxis(), AstronomicalUnit(0.387098310))
+        AssertEqual(mercury.inclination(.meanEquinoxOfTheDate(jd.date)), Degree(7.006171), accuracy: Degree(0.000001))
+        AssertEqual(mercury.longitudeOfAscendingNode(.meanEquinoxOfTheDate(jd.date)), Degree(49.107650), accuracy: Degree(0.000001))
+        AssertEqual(mercury.longitudeOfPerihelion(.meanEquinoxOfTheDate(jd.date)), Degree(78.475382), accuracy: Degree(0.000001))
+
+        // non-numeric types
+        XCTAssertEqualWithAccuracy(mercury.eccentricity(), 0.20564510, accuracy: 0.00000001)
+    }
+
+}
