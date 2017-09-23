065a8219acc65ba5ed7a9071355dc4010f5a75a2
onekiloparsec <cedric@onekilopars.ec>
2017-09-23 10:09:54 +0200

    style
    Signed-off-by: onekiloparsec <cedric@onekilopars.ec>

--- SwiftAATests/VenusTests.swift
+++ SwiftAATests/VenusTests.swift
@@ -16,18 +16,21 @@ class VenusTests: XCTestCase {
         XCTAssertNotEqual(Venus.averageColor, Color.white)
     }
     
-    func testApparentGeocentricCoordinates() { // See AA p.225
+    // See AA p.225
+    func testApparentGeocentricCoordinates() {
         let venus = Venus(julianDay: JulianDay(year: 1992, month: 12, day: 20))
         AssertEqual(Hour(venus.planetaryDetails.ApparentGeocentricRA), Hour(21.078181), accuracy: ArcSecond(0.1).inHours)
         AssertEqual(Degree(venus.planetaryDetails.ApparentGeocentricDeclination), Degree(-18.88801), accuracy: ArcSecond(0.1).inDegrees)
     }
     
-    func testIlluminationFraction() { // See AA p.284
+    // See AA p.284
+    func testIlluminationFraction() {
         let venus = Venus(julianDay: JulianDay(year: 1992, month: 12, day: 20))
         XCTAssertEqualWithAccuracy(venus.illuminatedFraction, 0.647, accuracy: 0.005)
     }
     
-    func testHeliocentricEclipticCoordinates() { // See AA p.225
+    // See AA p.225
+    func testHeliocentricEclipticCoordinates() {
         let venus = Venus(julianDay: JulianDay(year: 1992, month: 12, day: 20), highPrecision: false)
         let heliocentricEcliptic = venus.heliocentricEclipticCoordinates
         AssertEqual(heliocentricEcliptic.celestialLatitude, Degree(-2.62070), accuracy: ArcSecond(0.1).inDegrees)
@@ -35,11 +38,11 @@ class VenusTests: XCTestCase {
         AssertEqual(venus.radiusVector, AstronomicalUnit(0.724603), accuracy: AstronomicalUnit(0.00001))
     }
     
-    func testGeocentricEquatorialCoordinates() { // See AA p.103
+    // See AA p.103
+    func testGeocentricEquatorialCoordinates() {
         let venus = Venus(julianDay: JulianDay(year: 1988, month: 03, day: 20))
         let equatorial = venus.apparentGeocentricEquatorialCoordinates
         AssertEqual(equatorial.rightAscension.inDegrees, Degree(41.73129), accuracy: ArcSecond(0.1).inDegrees)
         AssertEqual(equatorial.declination, Degree(18.44092), accuracy: ArcSecond(0.1).inDegrees)
-    }
-    
+    }    
 }
