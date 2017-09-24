ba7280e2382801e8fd02196cb7c1a72c5e26133a
onekiloparsec <cedric@onekilopars.ec>
2017-09-24 10:05:06 +0200

    Basic test for coverage
    Signed-off-by: onekiloparsec <cedric@onekilopars.ec>

---
+++ SwiftAATests/AstronomicalObjectTests.swift
@@ -0,0 +1,19 @@
+//
+//  AstronomicalObjectTests.swift
+//  SwiftAA
+//
+//  Created by Cédric Foellmi on 2017-09-24.
+//  Copyright © 2017 onekiloparsec. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftAA
+
+class AstronomicalObjectTests: XCTestCase {
+
+    func testDummyRadiusVector() {
+        let coords = EquatorialCoordinates(alpha: Hour(.plus, 16, 54, 00.14), delta: Degree(.minus, 39, 50, 44.9))
+        AssertEqual(AstronomicalObject(name: "GRO J1655-40", coordinates: coords, julianDay: JulianDay(Date())).radiusVector, AstronomicalUnit(-1))
+    }
+
+}
