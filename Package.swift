81a60ba23f65eddbd7dc2af5e6b40092a892bd9b
Cédric Foellmi <cedric@onekiloparsec.dev>
2019-12-25 20:08:15 +0100

    Creating a SPM Package.swift file

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftAA",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v11),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ObjCAA",
+            targets: ["ObjCAA"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ObjCAA",
+            dependencies: [],
+            exclude: ["Sources/ObjCAA/ObjCAA-Info.plist"]
+        ),
+        .testTarget(
+            name: "ObjCAATests",
+            dependencies: ["ObjCAA"]),
+    ],
+    cxxLanguageStandard: .gnucxx14
+)
