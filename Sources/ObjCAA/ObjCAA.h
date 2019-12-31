ace4c7a371872f3c37b8a2461819d6e9ea7e30b0
Cédric Foellmi <cedric@onekiloparsec.dev>
2019-12-31 11:37:56 +0100

    Creating an ObjCAA podspec

---
+++ Sources/ObjCAA/ObjCAA.h
@@ -0,0 +1,77 @@
+//
+//  ObjCAA.h
+//  ObjCAA
+//
+//  Created by onekiloparsec on 25/12/2019.
+//  Copyright © 2019 onekiloparsec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjCAA.
+FOUNDATION_EXPORT double ObjCAAVersionNumber;
+
+//! Project version string for ObjCAA.
+FOUNDATION_EXPORT const unsigned char ObjCAAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjCAA/PublicHeader.h>
+
+#import <ObjCAA/KPCAASwiftAdditions.h>
+
+#import <ObjCAA/KPCAA2DCoordinate.h>
+#import <ObjCAA/KPCAA3DCoordinate.h>
+#import <ObjCAA/KPCAAAberration.h>
+#import <ObjCAA/KPCAAAngularSeparation.h>
+#import <ObjCAA/KPCAABinaryStar.h>
+#import <ObjCAA/KPCAACoordinateTransformation.h>
+#import <ObjCAA/KPCAADate.h>
+#import <ObjCAA/KPCAADiameters.h>
+#import <ObjCAA/KPCAADynamicalTime.h>
+#import <ObjCAA/KPCAAEarth.h>
+#import <ObjCAA/KPCAAEclipses.h>
+#import <ObjCAA/KPCAAEclipticalElements.h>
+#import <ObjCAA/KPCAAElementsPlanetaryOrbit.h>
+#import <ObjCAA/KPCAAElliptical.h>
+#import <ObjCAA/KPCAAEquationOfTime.h>
+#import <ObjCAA/KPCAAEquinoxesAndSolstices.h>
+#import <ObjCAA/KPCAAFK5.h>
+#import <ObjCAA/KPCAAGalileanMoons.h>
+#import <ObjCAA/KPCAAGlobe.h>
+#import <ObjCAA/KPCAAIlluminatedFraction.h>
+#import <ObjCAA/KPCAAInterpolate.h>
+#import <ObjCAA/KPCAAJupiter.h>
+#import <ObjCAA/KPCAAKepler.h>
+#import <ObjCAA/KPCAAMars.h>
+#import <ObjCAA/KPCAAMercury.h>
+#import <ObjCAA/KPCAAMoon.h>
+#import <ObjCAA/KPCAAMoonIlluminatedFraction.h>
+#import <ObjCAA/KPCAAMoonMaxDeclinations.h>
+#import <ObjCAA/KPCAAMoonNodes.h>
+#import <ObjCAA/KPCAAMoonPerigeeApogee.h>
+#import <ObjCAA/KPCAAMoonPhases.h>
+#import <ObjCAA/KPCAANearParabolic.h>
+#import <ObjCAA/KPCAANeptune.h>
+#import <ObjCAA/KPCAANodes.h>
+#import <ObjCAA/KPCAANutation.h>
+#import <ObjCAA/KPCAAParabolic.h>
+#import <ObjCAA/KPCAAParallactic.h>
+#import <ObjCAA/KPCAAParallax.h>
+#import <ObjCAA/KPCAAPhysicalJupiter.h>
+#import <ObjCAA/KPCAAPhysicalMars.h>
+#import <ObjCAA/KPCAAPhysicalMoon.h>
+#import <ObjCAA/KPCAAPhysicalSun.h>
+#import <ObjCAA/KPCAAPlanetPerihelionAphelion.h>
+#import <ObjCAA/KPCAAPlanetaryPhenomena.h>
+#import <ObjCAA/KPCAAPluto.h>
+#import <ObjCAA/KPCAAPrecession.h>
+#import <ObjCAA/KPCAARefraction.h>
+#import <ObjCAA/KPCAARiseTransitSet.h>
+#import <ObjCAA/KPCAASaturn.h>
+#import <ObjCAA/KPCAASaturnMoons.h>
+#import <ObjCAA/KPCAASaturnRings.h>
+#import <ObjCAA/KPCAASidereal.h>
+#import <ObjCAA/KPCAAStellarMagnitudes.h>
+#import <ObjCAA/KPCAASun.h>
+#import <ObjCAA/KPCAAUranus.h>
+#import <ObjCAA/KPCAAVenus.h>
+
