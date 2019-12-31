d81c2f8f78cd9cddc091c9da79be7073476e6364
Cédric Foellmi <cedric@onekiloparsec.dev>
2019-12-31 12:08:31 +0100

    Do we really need this ?

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AA+",
-            type: .static,
             targets: ["AA+"]
         ),
         .library(
