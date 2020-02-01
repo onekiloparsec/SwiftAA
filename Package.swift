82bb98c11b78d37589db8dd7b8de8369c3e9c92b
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-02-01 10:24:18 +0100

    Removing invalid character in bundle name AA+ -> AAplus

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "AA+",
+            name: "AAplus",
             type: .dynamic,
-            targets: ["AA+"]
+            targets: ["AAplus"]
         ),
         .library(
             name: "ObjCAA",
@@ -30,13 +30,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "AA+",
+            name: "AAplus",
             dependencies: [],
+            path: "Sources/AA+",
             exclude: ["Sources/AA+/naughter.css", "Sources/AA+/CMakeLists.txt", "Sources/AA+/AA+.htm"]
         ),
         .target(
             name: "ObjCAA",
-            dependencies: ["AA+"],
+            dependencies: ["AAplus"],
             exclude: []
         ),
         .target(
