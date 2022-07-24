f12f733fbb61289dc5ab8165df775f09f9d0f4b2
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-07-24 19:12:28 +0200

    Adjusting versions to be consistent with project
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftAA",
     platforms: [
-        .macOS(.v10_11), .iOS(.v11),
+        .macOS(.v10_13), .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
